320123b9f9cc0783a9ecd2ada42c9e18bbf439c8
Hakop Zarikyan <hakop2006@gmail.com>
2017-07-16 14:11:34 -0700

    Added timespan

--- XnaPlatformer/XnaPlatformer/Game1.cs
+++ XnaPlatformer/XnaPlatformer/Game1.cs
@@ -19,6 +19,8 @@ namespace XnaPlatformer
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        TimeSpan startSpan;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
